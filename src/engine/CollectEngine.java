ce77378f78245126b9756d38d1e76993e9d34186
Francesco Magno <francesco@magno.cc>
2016-12-04 23:56:11 +0100

    work in progress

--- src/engine/CollectEngine.java
+++ src/engine/CollectEngine.java
@@ -9,16 +9,28 @@ import engine.samples.LoadSample;
 import engine.samples.MemSample;
 import engine.samples.NetSample;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import static main.JCollectd.logger;
 import static main.JCollectd.prettyPrint;
 
@@ -31,6 +43,10 @@ public class CollectEngine {
     private CollectResult prevResult;
     private CollectResult curResult;
 
+    // dates
+    private final SimpleDateFormat sdfms = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
     // jdbc
     private static final String CREATE_TB_STMT = "CREATE TABLE IF NOT EXISTS probe_samples (id_sample INTEGER PRIMARY KEY, probe_name TEXT, sample_tms TEXT, sample_value TEXT)";
     private static final String CREATE_IDX_STMT = "CREATE UNIQUE INDEX IF NOT EXISTS idx_probe_samples ON probe_samples (probe_name, sample_tms)";
@@ -38,7 +54,14 @@ public class CollectEngine {
     private static final String BEGIN_TRANS = "BEGIN TRANSACTION";
     private static final String END_TRANS = "END TRANSACTION";
     private static final String PRAGMA = "PRAGMA busy_timeout = 5000";
-    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+    private static final String SELECT_LOAD = "SELECT sample_tms,\n"
+            + "MAX(CASE WHEN probe_name = 'load1m' THEN sample_value ELSE NULL END) l1m,\n"
+            + "MAX(CASE WHEN probe_name = 'load5m' THEN sample_value ELSE NULL END) l5m,\n"
+            + "MAX(CASE WHEN probe_name = 'load15m' THEN sample_value ELSE NULL END) l15m\n"
+            + "FROM probe_samples\n"
+            + "WHERE sample_tms > ?\n"
+            + "AND (probe_name = 'load1m' OR probe_name = 'load5m' OR probe_name = 'load15m')\n"
+            + "GROUP BY sample_tms ORDER BY sample_tms;";
 
     public CollectEngine(CollectConfig conf) {
         this.conf = conf;
@@ -58,13 +81,14 @@ public class CollectEngine {
             // making room for new samples
             prevResult = curResult;
             curResult = new CollectResult(conf.getProbeConfigList().size());
+            long startCollectTime, endCollectTime, startSaveTime, endSaveTime, startReportTime, endReportTime;
 
             // collecting samples
             if (Thread.currentThread().isInterrupted()) {
                 logger.info("Received KILL signal, exiting from main loop");
                 return;
             }
-            long startCollectTime = System.nanoTime();
+            startCollectTime = System.nanoTime();
             for (int i = 0; i < conf.getProbeConfigList().size(); i++) {
                 if (conf.getProbeConfigList().get(i).getPtype() == ProbeType.LOAD) {
                     curResult.getProbeSampleList().add(i, parseLoadAvg());
@@ -79,7 +103,7 @@ public class CollectEngine {
                 }
                 logger.debug(curResult.getProbeSampleList().get(i).toString());
             }
-            long endCollectTime = System.nanoTime();
+            endCollectTime = System.nanoTime();
             logger.info("Collecting time: {} msec", prettyPrint((endCollectTime - startCollectTime) / 1000000L));
 
             // saving results
@@ -88,7 +112,7 @@ public class CollectEngine {
                 return;
             }
             if (prevResult != null) {
-                long startSaveTime = System.nanoTime();
+                startSaveTime = System.nanoTime();
                 try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + conf.getDbPath().toString())) {
                     try (Statement stmt = conn.createStatement()) {
                         stmt.executeUpdate(PRAGMA);
@@ -97,7 +121,7 @@ public class CollectEngine {
                     }
                     try (Statement stmt = conn.createStatement(); PreparedStatement pstmt = conn.prepareStatement(INS_STMT)) {
                         stmt.executeUpdate(BEGIN_TRANS);
-                        String sample_tms = sdf.format(curResult.getCollectTms());
+                        String sample_tms = sdfms.format(curResult.getCollectTms());
                         long elapsedMsec = curResult.getCollectTms().getTime() - prevResult.getCollectTms().getTime();
                         for (int i = 0; i < conf.getProbeConfigList().size(); i++) {
                             if (conf.getProbeConfigList().get(i).getPtype() == ProbeType.LOAD) {
@@ -175,8 +199,64 @@ public class CollectEngine {
                     logger.fatal("Error while saving samples to DB, aborting - {}", ex.getMessage());
                     return;
                 }
-                long endSaveTime = System.nanoTime();
+                endSaveTime = System.nanoTime();
                 logger.info("Saving time: {} msec", prettyPrint((endSaveTime - startSaveTime) / 1000000L));
+            }
+
+            // generating report
+            if (Thread.currentThread().isInterrupted()) {
+                logger.info("Received KILL signal, exiting from main loop");
+                return;
+            }
+            if (prevResult != null) {
+                startReportTime = System.nanoTime();
+                try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(conf.getWebPath().toString()), Charset.forName("UTF-8"), new OpenOption[]{WRITE, CREATE, TRUNCATE_EXISTING});
+                        Connection conn = DriverManager.getConnection("jdbc:sqlite:" + conf.getDbPath().toString())) {
+                    try (Statement stmt = conn.createStatement()) {
+                        stmt.executeUpdate(PRAGMA);
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    // header
+                    sb.append(writeHeader(conf.getHostname()));
+
+                    // calculate reporting window
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(curResult.getCollectTms());
+                    cal.add(Calendar.HOUR_OF_DAY, -conf.getReportHours());
+                    String fromTime = sdf.format(cal.getTime());
+
+                    // samples
+                    for (int i = 0; i < conf.getProbeConfigList().size(); i++) {
+                        if (conf.getProbeConfigList().get(i).getPtype() == ProbeType.LOAD) {
+                            sb.append(writeLoad(conn, fromTime));
+                        }
+                    }
+                    sb.append("</script>").append(System.lineSeparator());
+                    sb.append("</head>\n"
+                            + "    <body>\n"
+                            + "        <div class=\"navbar\">\n"
+                            + "            <span class=\"navlenft\"><i class=\"fa fa-area-chart\"></i> DarkSun</span>\n"
+                            + "            <span class=\"navright\"><i class=\"fa fa-clock-o\"></i> 04/12/2016 10:25:01</span>\n"
+                            + "        </div>\n"
+                            + "        <div class=\"main-container\">\n"
+                            + "<div class=\"chart-container\" id=\"div_load\"></div>"
+                            + "            <div class=\"footer\">\n"
+                            + "                <p>Time spent collecting samples: 24ms, writing samples: 484ms, reading samples: 56ms, generating report: 11ms</p>\n"
+                            + "            </div>\n"
+                            + "        </div>\n"
+                            + "    </body>\n"
+                            + "</html>").append(System.lineSeparator());
+                    String report = sb.toString();
+                    bw.write(report);
+                } catch (IOException ex) {
+                    logger.fatal("I/O error while generating HTML report, aborting - {}", ex.getMessage());
+                    return;
+                } catch (SQLException ex) {
+                    logger.fatal("Error while reading samples from DB, aborting - {}", ex.getMessage());
+                    return;
+                }
+                endReportTime = System.nanoTime();
+                logger.info("Reporting time: {} msec", prettyPrint((endReportTime - startReportTime) / 1000000L));
             }
         }
     }
@@ -304,4 +384,65 @@ public class CollectEngine {
         return ret;
     }
 
+    private String writeHeader(String hostname) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/header.html"), "UTF-8"))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append(System.lineSeparator());
+            }
+            sb.append(System.lineSeparator());
+            return sb.toString().replaceFirst("REPLACEME", hostname);
+        }
+    }
+
+    private String writeLoad(Connection conn, String fromTime) throws SQLException, IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("google.charts.setOnLoadCallback(drawLoad);").append(System.lineSeparator());
+        sb.append("function drawLoad() {").append(System.lineSeparator());
+        sb.append("var data = new google.visualization.DataTable();").append(System.lineSeparator());
+        sb.append("data.addColumn('datetime', 'Time');").append(System.lineSeparator());
+        sb.append("data.addColumn('number', '1 min');").append(System.lineSeparator());
+        sb.append("data.addColumn('number', '5 min');").append(System.lineSeparator());
+        sb.append("data.addColumn('number', '15 min');").append(System.lineSeparator());
+
+        try (PreparedStatement stmt = conn.prepareStatement(SELECT_LOAD)) {
+            stmt.setString(1, fromTime);
+            try (ResultSet rs = stmt.executeQuery()) {
+                Calendar cal = Calendar.getInstance();
+                while (rs.next()) {
+                    try {
+                        cal.setTime(sdf.parse(rs.getString(1)));
+                    } catch (ParseException ex) {
+                        // can't happen, but just in case we skip the line
+                        continue;
+                    }
+                    sb.append("data.addRows([[new Date(");
+                    sb.append(cal.get(Calendar.YEAR)).append(",");
+                    sb.append(cal.get(Calendar.MONTH) + 1).append(",");
+                    sb.append(cal.get(Calendar.DAY_OF_MONTH)).append(",");
+                    sb.append(cal.get(Calendar.HOUR_OF_DAY)).append(",");
+                    sb.append(cal.get(Calendar.MINUTE)).append(",");
+                    sb.append(cal.get(Calendar.SECOND)).append("),");
+                    sb.append(rs.getString(2)).append(",");
+                    sb.append(rs.getString(3)).append(",");
+                    sb.append(rs.getString(4));
+                    sb.append("]]);");
+                    sb.append(System.lineSeparator());
+                }
+            }
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/options_load.js"), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append(System.lineSeparator());
+            }
+        }
+
+        sb.append("var chart = new google.visualization.AreaChart(document.getElementById('div_load'));").append(System.lineSeparator());
+        sb.append("chart.draw(data, options);").append(System.lineSeparator());
+        sb.append("}").append(System.lineSeparator());
+        return sb.toString();
+    }
 }
