52b99a31b48e338a92c434daa17c0f61677e628f
Francesco Magno <francesco@magno.cc>
2016-12-05 09:30:15 +0100

    work

---
+++ src/options_load.js
@@ -0,0 +1,35 @@
+var date_formatter = new google.visualization.DateFormat({pattern: 'dd/MM/yyyy HH:mm:ss'});
+date_formatter.format(data, 0);
+var options = {
+    title: 'Average Load',
+    colors: ['#3366cc', '#ff9900', '#dc3912'],
+    backgroundColor: {fill: 'transparent'},
+    height: 350,
+    fontName: 'Roboto',
+    titleTextStyle: {color: '#eeeeee', fontSize: 16, bold: false},
+    legend: {
+        textStyle: {color: '#eeeeee'},
+        position: 'bottom'
+    },
+    hAxis: {
+        textStyle: {color: '#eeeeee'},
+        format: 'HH:mm'
+    },
+    vAxis: {
+        textStyle: {color: '#eeeeee'},
+        format: '#0.00'
+    },
+    tooltip: {
+        textStyle: {color: '#082235'}
+    },
+    animation: {
+        duration: 500,
+        easing: 'linear',
+        startup: true
+    },
+    chartArea: {
+        width: '100%',
+        left: 75,
+        right: 50
+    }
+};
